44cc1c5ff6bd132e8c95f1b53f82e9eb042a1e52
Rafael <rafaelnunesmoura@protonmial.com>
2024-12-03 17:07:56 -0300

    Initial commit

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:untitled/components/button.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 168, 60, 55),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const SizedBox(
+              height: 25.0,
+            ),
+
+            // Title
+             Text(
+              "Sushi Man",
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+
+            ),
+            const SizedBox(height: 25),
+
+            // Image
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Image.asset('lib/images/sushi (1).png'),
+            ),
+            const SizedBox(
+              height: 25.0
+            ),
+
+            // Subtitle
+             Text(
+              "THE TASTE  OF JAPANESE  FOOD",
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 44,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Text(
+              "Feel the taste of the must popular Japanese Food from anywhere and anytime",
+              style: TextStyle(
+                height: 2,
+                color: Colors.grey[200],
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            MyButton(
+              text: "Get Started",
+              onTap: (){
+              // Go to Main Page
+              Navigator.pushNamed(context,'/menupage');
+              },
+
+            ),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
